921f5db321fec27d1d8bdeb09cc2bef0851d26e5
robot-den <w.nazmutdinov@gmail.com>
2016-07-26 23:07:37 +0500

    refactored

--- target_my.rb
+++ target_my.rb
@@ -2,75 +2,75 @@ require 'curb'
 require 'json'
 
 class TargetMy
-	def solution(login, password, app_link, app_name)
-		request = auth(login, password)
-		app_id = create_app(request, app_link, app_name)
-		create_fullscreen_ad_unit(request, app_id)
-		get_slot_ids(request, app_id)
-	end
+  def solution(login, password, app_link, app_name)
+    request = auth(login, password)
+    app_id = create_app(request, app_link, app_name)
+    create_fullscreen_ad_unit(request, app_id)
+    get_slot_ids(request, app_id)
+  end
 
-	private
+  private
 
-	def auth(login, password)
-		request = Curl::Easy.new("https://auth-ac.my.com/auth")
-		request.verbose = true
-		request.enable_cookies = true
-		request.headers['User-Agent'] = 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:42.0) Gecko/20100101 Firefox/42.0'
-		request.headers['Origin'] = "https://account.my.com"
-		request.headers['Referer'] = "https://account.my.com/login/?continue=https://account.my.com/profile/userinfo/"
-		request.http_post("email=#{ login }&password=#{ password }")
-		request
-	end
+  def auth(login, password)
+    request = Curl::Easy.new('https://auth-ac.my.com/auth')
+    request.verbose = true
+    request.enable_cookies = true
+    request.headers['User-Agent'] = 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:42.0) Gecko/20100101 Firefox/42.0'
+    request.headers['Origin'] = 'https://account.my.com'
+    request.headers['Referer'] = 'https://account.my.com/login/?continue=https://account.my.com/profile/userinfo/'
+    request.http_post("email=#{login}&password=#{password}")
+    request
+  end
 
-	def create_app(request, app_link, app_name, first_block_name='standard')
-		request.follow_location = true
-		request.url = 'https://target.my.com/create_pad_groups/'
-		request.http_get
-		request.follow_location = false
-		request.url = 'https://target.my.com/api/v2/pad_groups.json'
-		request.headers['X-CSRFToken'] = request.header_str.match(/(?<=csrftoken=)(.*?)(?=;)/)
-		request.headers['Origin'] = "https://target.my.com"
-		request.headers['Referer'] = "https://target.my.com/create_pad_groups/"
-		request.http_post("{\"url\":\"#{ app_link }\",\"platform_id\":6122,\"description\":\"#{ app_name }\",\"pads\":[{\"description\":\"#{ first_block_name }\",\"format_id\":6124,\"filters\":{\"deny_mobile_android_category\":[],\"deny_mobile_category\":[],\"deny_topics\":[],\"deny_pad_url\":[]},\"js_tag\":false,\"shows_period\":\"day\",\"shows_limit\":null,\"shows_interval\":null}]}")
-		app_id = JSON.parse(request.body_str)['id']
-	end
+  def create_app(request, app_link, app_name, first_block_name = 'standard')
+    request.follow_location = true
+    request.url = 'https://target.my.com/create_pad_groups/'
+    request.http_get
+    request.follow_location = false
+    request.url = 'https://target.my.com/api/v2/pad_groups.json'
+    request.headers['X-CSRFToken'] = request.header_str.match(/(?<=csrftoken=)(.*?)(?=;)/)
+    request.headers['Origin'] = 'https://target.my.com'
+    request.headers['Referer'] = 'https://target.my.com/create_pad_groups/'
+    request.http_post("{\"url\":\"#{ app_link }\",\"platform_id\":6122,\"description\":\"#{app_name}\",\"pads\":[{\"description\":\"#{first_block_name}\",\"format_id\":6124,\"filters\":{\"deny_mobile_android_category\":[],\"deny_mobile_category\":[],\"deny_topics\":[],\"deny_pad_url\":[]},\"js_tag\":false,\"shows_period\":\"day\",\"shows_limit\":null,\"shows_interval\":null}]}")
+    JSON.parse(request.body_str)['id']
+  end
 
-	def create_fullscreen_ad_unit(request, app_id)
-		request.follow_location = true
-		request.url = "https://target.my.com/pad_groups/#{ app_id }/create/"
-		request.http_get
-		request.follow_location = false
-		request.url = "https://target.my.com/api/v2/pad_groups/#{ app_id }/pads.json"
-		request.headers['X-CSRFToken'] = request.header_str.match(/(?<=csrftoken=)(.*?)(?=;)/)
-		request.headers['Origin'] = "https://target.my.com"
-		request.headers['Referer'] = "https://target.my.com/pad_groups/#{ app_id }/create/"
-		request.http_post("{\"description\":\"fullscreen\",\"format_id\":6440,\"filters\":{\"deny_mobile_android_category\":[],\"deny_mobile_category\":[],\"deny_topics\":[],\"deny_pad_url\":[]},\"js_tag\":false,\"shows_limit\":null,\"shows_interval\":null}")
-	end
+  def create_fullscreen_ad_unit(request, app_id)
+    request.follow_location = true
+    request.url = "https://target.my.com/pad_groups/#{app_id}/create/"
+    request.http_get
+    request.follow_location = false
+    request.url = "https://target.my.com/api/v2/pad_groups/#{app_id}/pads.json"
+    request.headers['X-CSRFToken'] = request.header_str.match(/(?<=csrftoken=)(.*?)(?=;)/)
+    request.headers['Origin'] = 'https://target.my.com'
+    request.headers['Referer'] = "https://target.my.com/pad_groups/#{app_id}/create/"
+    request.http_post("{\"description\":\"fullscreen\",\"format_id\":6440,\"filters\":{\"deny_mobile_android_category\":[],\"deny_mobile_category\":[],\"deny_topics\":[],\"deny_pad_url\":[]},\"js_tag\":false,\"shows_limit\":null,\"shows_interval\":null}")
+  end
 
-	def get_slot_ids(request, app_id)
-		request.follow_location = true
-		links = get_ad_unit_links(request, app_id)
-		result = []
-		links.each do |link|
-		  request.url = link
-		  request.http_get
-		  response = JSON.parse(request.body_str)
-		  format = case response['format_id']
-		    when 6124
-		      'standard'
-		    when 6440
-		      'fullscreen'
-		    else
-		      "#{ response['format_id'] } is undefined"
-		  end
-		  result << [format, response['slot_id']]
-		end
-		result
-	end
+  def get_slot_ids(request, app_id)
+    request.follow_location = true
+    links = get_ad_unit_links(request, app_id)
+    result = []
+    links.each do |link|
+      request.url = link
+      request.http_get
+      response = JSON.parse(request.body_str)
+      format = case response['format_id']
+               when 6124
+                 'standard'
+               when 6440
+                 'fullscreen'
+               else
+                 "#{response['format_id']} is undefined"
+               end
+      result << [format, response['slot_id']]
+    end
+    result
+  end
 
-	def get_ad_unit_links(request, app_id)
-		request.url = "https://target.my.com/api/v2/pad_groups/#{ app_id }.json"
-		request.http_get
-		links = JSON.parse(request.body_str)['pads'].collect { |pad| "https://target.my.com/api/v1/pads/#{ pad['id'] }.json" }
-	end
+  def get_ad_unit_links(request, app_id)
+    request.url = "https://target.my.com/api/v2/pad_groups/#{app_id}.json"
+    request.http_get
+    JSON.parse(request.body_str)['pads'].collect { |pad| "https://target.my.com/api/v1/pads/#{pad['id']}.json" }
+  end
 end
