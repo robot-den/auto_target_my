e4a35b52f09500ef2829985b5ec348faf33dd933
robot-den <w.nazmutdinov@gmail.com>
2016-07-27 22:48:07 +0500

    rebuild

--- lib/target_my.rb
+++ lib/target_my.rb
@@ -2,65 +2,57 @@ require 'curb'
 require 'json'
 
 class TargetMy
-  def solution(login, password, app_link, app_name)
-    request = auth(login, password)
-    app_id = create_app(request, app_link, app_name)
-    create_fullscreen_ad_unit(request, app_id)
-    get_slot_ids(request, app_id)
+
+  attr_accessor :login, :password, :app_link, :app_name, :app_id
+
+  def initialize(login, password, app_link, app_name)
+    @login = login
+    @password = password
+    @app_link = app_link
+    @app_name = app_name
+    @app_id = nil
   end
 
-  private
-
-  def auth(login, password)
-    puts 'Логинимся'
-    request = Curl::Easy.new('https://auth-ac.my.com/auth')
-    request.enable_cookies = true
-    request.headers['User-Agent'] = 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:42.0) Gecko/20100101 Firefox/42.0'
-    request.headers['Origin'] = 'https://account.my.com'
-    request.headers['Referer'] = 'https://account.my.com/profile/userinfo/'
-    request.http_post("email=#{login}&password=#{password}")
-    raise 'Auth failed' if request.redirect_url != 'http://mail.my.com/'
-    request
-  end
-
-  def create_app(request, app_link, app_name, first_block_name = 'standard')
-    puts 'Добавляем приложение'
-    # собираем cookies
+  def create_and_bind_app
+    raise 'Application already exists and attached' unless @app_id.nil?
+    request = auth
     request.follow_location = true
     request.url = 'https://target.my.com/create_pad_groups/'
     request.http_get
-    # настраиваем и отправляем запрос
     request.follow_location = false
     request.url = 'https://target.my.com/api/v2/pad_groups.json'
     request.headers['X-CSRFToken'] = request.header_str.match(/(?<=csrftoken=)(.*?)(?=;)/)
     request.headers['Origin'] = 'https://target.my.com'
     request.headers['Referer'] = 'https://target.my.com/create_pad_groups/'
-    request.http_post("{\"url\":\"#{app_link}\",\"platform_id\":6122,\"description\":\"#{app_name}\",\"pads\":[{\"description\":\"#{first_block_name}\",\"format_id\":6124,\"filters\":{\"deny_mobile_android_category\":[],\"deny_mobile_category\":[],\"deny_topics\":[],\"deny_pad_url\":[]},\"js_tag\":false,\"shows_period\":\"day\",\"shows_limit\":null,\"shows_interval\":null}]}")
+    request.http_post("{\"url\":\"#{@app_link}\",\"platform_id\":6122,\"description\":\"#{@app_name}\",\"pads\":[{\"description\":\"standart\",\"format_id\":6124,\"filters\":{\"deny_mobile_android_category\":[],\"deny_mobile_category\":[],\"deny_topics\":[],\"deny_pad_url\":[]},\"js_tag\":false,\"shows_period\":\"day\",\"shows_limit\":null,\"shows_interval\":null}]}")
     raise "Creating app failed with status #{request.status}" if request.status != '200 OK'
-    JSON.parse(request.body_str)['id']
+    @app_id = JSON.parse(request.body_str)['id']
   end
 
-  def create_fullscreen_ad_unit(request, app_id)
-    puts 'Добавляем fullscreen блок'
-    # собираем cookies
+  def add_fullscreen_block
+    raise 'Application does not created' if @app_id.nil?
+    request = auth
     request.follow_location = true
-    request.url = "https://target.my.com/pad_groups/#{app_id}/create/"
+    request.url = "https://target.my.com/pad_groups/#{@app_id}/create/"
     request.http_get
-    # настраиваем и отправляем запрос
     request.follow_location = false
-    request.url = "https://target.my.com/api/v2/pad_groups/#{app_id}/pads.json"
+    request.url = "https://target.my.com/api/v2/pad_groups/#{@app_id}/pads.json"
     request.headers['X-CSRFToken'] = request.header_str.match(/(?<=csrftoken=)(.*?)(?=;)/)
     request.headers['Origin'] = 'https://target.my.com'
-    request.headers['Referer'] = "https://target.my.com/pad_groups/#{app_id}/create/"
+    request.headers['Referer'] = "https://target.my.com/pad_groups/#{@app_id}/create/"
     request.http_post("{\"description\":\"fullscreen\",\"format_id\":6440,\"filters\":{\"deny_mobile_android_category\":[],\"deny_mobile_category\":[],\"deny_topics\":[],\"deny_pad_url\":[]},\"js_tag\":false,\"shows_limit\":null,\"shows_interval\":null}")
     raise "Creating ad unit failed with status #{request.status}" if request.status != '200 OK'
   end
 
-  def get_slot_ids(request, app_id)
-    puts 'Генерируем список slot_id'
+  def slot_ids
+    raise 'Application does not created' if @app_id.nil?
+    request = auth
     request.follow_location = true
-    links = get_ad_unit_links(request, app_id)
-    result = []
+    request.url = "https://target.my.com/api/v2/pad_groups/#{@app_id}.json"
+    request.http_get
+    links = JSON.parse(request.body_str)['pads'].collect { |pad| "https://target.my.com/api/v1/pads/#{pad['id']}.json" }
+
+    result = {}
     links.each do |link|
       request.url = link
       request.http_get
@@ -73,15 +65,23 @@ class TargetMy
                else
                  "#{response['format_id']} is undefined"
                end
-      result << [format, response['slot_id']]
+      result[response['slot_id']] = format
     end
     result
   end
 
-  def get_ad_unit_links(request, app_id)
-    puts 'Получаем ссылки на блоки'
-    request.url = "https://target.my.com/api/v2/pad_groups/#{app_id}.json"
-    request.http_get
-    JSON.parse(request.body_str)['pads'].collect { |pad| "https://target.my.com/api/v1/pads/#{pad['id']}.json" }
+  private
+
+  def auth
+    @request ||= begin
+      request = Curl::Easy.new('https://auth-ac.my.com/auth')
+      request.enable_cookies = true
+      request.headers['User-Agent'] = 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:42.0) Gecko/20100101 Firefox/42.0'
+      request.headers['Origin'] = 'https://account.my.com'
+      request.headers['Referer'] = 'https://account.my.com/profile/userinfo/'
+      request.http_post("email=#{@login}&password=#{@password}")
+      raise 'Auth failed' if request.redirect_url != 'http://mail.my.com/'
+      request
+    end
   end
 end
--- spec/target_my_spec.rb
+++ spec/target_my_spec.rb
@@ -1,29 +1,63 @@
 require 'target_my'
 
 describe TargetMy do
-  describe '#solution' do
-    let(:app_link) { 'https://itunes.apple.com/en/app/angry-birds/id343200656?mt=8' }
-    let(:app_name) { 'test_app' }
-    let(:result) { TargetMy.new.solution(login, password, app_link, app_name) }
+  let(:app_link) { 'https://itunes.apple.com/en/app/angry-birds/id343200656?mt=8' }
+  let(:app_name) { 'test_app' }
+  let(:login) { '' }
+  let(:password) { '' }
+  let(:instance) { TargetMy.new(login, password, app_link, app_name) }
 
-    context 'with correct login and password' do
-      let(:login) { 'mailfortestapp@mail.ru' }
-      let(:password) { '12345678d' }
-
-      it 'return array with slot id of ad units' do
-        expect(result[0][0]).to eq 'standard'
-        expect(result[0][1]).to be_a Fixnum
-        expect(result[1][0]).to eq 'fullscreen'
-        expect(result[1][1]).to be_a Fixnum
-      end
-    end
-
+  describe '#create_and_bind_app' do
     context 'with incorrect login and password' do
       let(:login) { 'oohyoutouchmytalala' }
       let(:password) { 'dingdingdong' }
 
       it 'raise error' do
-        expect { result }.to raise_error RuntimeError, 'Auth failed'
+        expect { instance.create_and_bind_app }.to raise_error RuntimeError, 'Auth failed'
+      end
+
+      it 'does not set app_id for instance' do
+        expect { instance.create_and_bind_app rescue nil }.to_not change(instance, :app_id)
+      end
+    end
+
+    context 'with correct login and password' do
+      it 'set app_id for instance' do
+        instance.create_and_bind_app
+        expect(instance).to have_attributes(:app_id => an_instance_of(Fixnum))
+      end
+
+      context 'if instance already have app' do
+        it 'raise error' do
+          instance.create_and_bind_app
+          expect { instance.create_and_bind_app }.to raise_error RuntimeError, 'Application already exists and attached'
+        end
+      end
+    end
+  end
+
+  describe '#add_fullscreen_block' do
+    context 'if app does not added' do
+      it 'raise error' do
+        expect { instance.add_fullscreen_block }.to raise_error RuntimeError, 'Application does not created'
+      end
+    end
+  end
+
+  describe '#slot_ids' do
+    context 'if app does not added' do
+      it 'raise error' do
+        expect { instance.add_fullscreen_block }.to raise_error RuntimeError, 'Application does not created'
+      end
+    end
+
+    context 'if app added' do
+      it 'return hash with slot_ids and types of blocks' do
+        instance.create_and_bind_app
+        hash = instance.slot_ids
+        expect(hash).to be_a Hash
+        expect(hash.keys[0]).to be_a Fixnum
+        expect(hash.values[0]).to eq 'standard'
       end
     end
   end
